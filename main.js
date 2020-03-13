a0f8bb8d74d713ea77362d208abb7a04b10de793
ranchTrash <ag.saul99@gmail.com>
2020-03-13 17:12:45 -0600

    1

---
+++ main.js
@@ -0,0 +1,18 @@
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+
+// // viewed at http://localhost:8080
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/views/index.html'));
+// });
+
+// // app.use(express.static('public'));
+// // app.use('/static', express.static(__dirname + '/public'));
+
+// // app.use('/js', express.static(__dirname + '/public/js'));
+
+// app.use(express.static('/public'));
+
+// app.listen(8080);
+
